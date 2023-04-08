419797f8e98cf6c4593352a1ca0cc602c54a0f67
githubscientist <sathish.datascientist@gmail.com>
2023-04-08 19:00:59 +0530

    linked list completed

--- day3/LinkedList.js
+++ day3/LinkedList.js
@@ -10,41 +10,31 @@ class LinkedList {
         this.head = null;
     }
 
+    // insertAtTail
     push(data) {
-        // creating a new node
         let newNode = new Node(data);
-
         if (this.head == null) {
-            // check whether the list is empty
             this.head = newNode;
         } else {
-            // the list has already some elements
-            // creating the temporary head
             let thead = this.head;
-
-            // find the last node of the linked list
             while (thead.next != null) {
-                // go to the next node
                 thead = thead.next;
             }
-
-            // attach the newnode as the next node to the last node in the linked list
             thead.next = newNode;
         }
     }
 
     toString() {
         let nodes = [];
-
         let thead = this.head;
         while (thead != null) {
             nodes.push(thead.data);
             thead = thead.next;
         }
-
         console.log(nodes); 
     }
 
+    // deleteAtTail
     pop() {
         if (this.head == null) {
             console.log('list is empty!');
@@ -52,7 +42,6 @@ class LinkedList {
             this.head = null;
         } else {
             let thead = this.head;
-
             while (thead.next.next != null) {
                 thead = thead.next;
             }
@@ -61,9 +50,41 @@ class LinkedList {
     }
 
     length() {
-        // return the length of the linked list
-        // return the number of nodes in the linked list
+        let thead = this.head;
+        let nodes = 0;
+        while (thead != null) {
+            nodes++;
+            thead = thead.next;
+        }
+        return nodes;
+    }
+
+    unshift(data) {
+        let newNode = new Node(data);
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    }
+
+    shift() {
+        if (this.head == null) {
+            console.log('list is empty!');
+        } else {
+            this.head = this.head.next;
+        }
+    }
+
+    splice(pos, data) {
+        // insert the data at the position pos of the linked list
         
+    }
+
+    middleNode() {
+        // return the middle node data of the linked list
+
     }
 }
 
@@ -73,6 +94,14 @@ let list = new LinkedList();
 list.push(4);
 list.push(5);
 list.push(6);
+list.push(55);
+list.unshift(11);
+list.unshift(10);
+list.push(7);
+list.pop();
+list.shift();
+list.shift();
+list.pop();
 
-
-console.log(list.length());
+// console.log(list.length());
+list.toString();
